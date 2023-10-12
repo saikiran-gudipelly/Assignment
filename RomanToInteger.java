346cd9616fc529621c4179adaf7294f200ad253c
saikiran-gudipelly <146613622+saikiran-gudipelly@users.noreply.github.com>
2023-10-12 17:35:04 +0530

    Add files via upload

---
+++ RomanToInteger.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class RomanToInteger {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a Roman numeral: ");
+        String romanNumeral = scanner.nextLine().toUpperCase(); // Convert to uppercase for case insensitivity
+
+        int result = romanToInteger(romanNumeral);
+        if (result != -1) {
+            System.out.println("The integer value of " + romanNumeral + " is " + result);
+        } else {
+            System.out.println("Invalid Roman numeral.");
+        }
+    }
+
+    public static int romanToInteger(String s) {
+        Map<Character, Integer> romanMap = new HashMap<>();
+        romanMap.put('I', 1);
+        romanMap.put('V', 5);
+        romanMap.put('X', 10);
+        romanMap.put('L', 50);
+        romanMap.put('C', 100);
+        romanMap.put('D', 500);
+        romanMap.put('M', 1000);
+
+        int result = 0;
+        int prevValue = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int value = romanMap.get(s.charAt(i));
+            if (value < prevValue) {
+                result -= value;
+            } else {
+                result += value;
+            }
+            prevValue = value;
+        }
+
+        return result;
+    }
+}
